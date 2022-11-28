924dbc1a8f393b310b0fdcaa254a7461dfea55a2
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-11-28 23:43:04 +0000

    Add files via upload

---
+++ Assignment2ef.sql
@@ -0,0 +1,14 @@
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(1,'BGL',18-06-2017,400.65,2);
+	insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(2,'HYD',05-10-2017,600.00,3);
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(3,'CHN',25-07-2016,445.95,3);
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(4,'PUN',10-12-2017,543.00,4);
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(5,'MUN',28-01-2017,500.50,4);
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(6,'PUN',27-03-2026,333.55,3);
+insert into BusInfo(BusId, BoardingPoint, TravelDate, Amount,Rating)
+    values(7,'MUM',06-11-2016,510.00,4);
