8ad2cc768ef4a8047165bb41dc0d980c0369596b
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-11-29 09:30:25 +0530

    Create Assignment1storedprocessor.sql

---
+++ Assignment1storedprocessor.sql
@@ -0,0 +1,12 @@
+Create procedure InsertFootBallLeague4
+@MatchID int,
+@TeamName1 nvarchar(30),
+@TeamName2 nvarchar(30),
+@Status varchar(10),
+@WinningTeam nvarchar(30),
+@Points int
+as
+Begin
+Insert into FootBallLeague4 (MatchID,TeamName1,TeamName2,Status,WinningTeam,Points)
+values (@MatchID, @TeamName1, @TeamName2, @Status, @WinningTeam, @Points)
+End;
